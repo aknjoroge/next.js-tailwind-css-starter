64d8f16a332d019a88acb419a99901fb77db2984
aknjoroge <alexkaranja629@gmail.com>
2023-09-05 10:58:00 +0300

    starter active

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,15 @@ export default function Home() {
         <meta name="description" content="Get started with tailwind" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="mt-8">
+        <div className="p-6 max-w-xl mx-auto bg-white rounded-lg shadow-md flex items-center space-x-8">
+          <div className="shrink-0">Image</div>
+          <div>
+            <div className="text-xl font-medium text-black ">ChitChat</div>
+            <p className="text-slate-500">You have a new message!</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
